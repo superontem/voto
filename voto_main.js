da8b999692f9412ac9ce7b9d574d9dd4f9cd3229
Arthur <arthur_rosa2@estudante.sesisenai.org.br>
2024-02-08 08:57:15 -0300

    leitura dos dados do formulario

---
+++ voto_main.js
@@ -0,0 +1,11 @@
+let resposta = document.getElementById('resposta')
+let verificar = document.getElementById('verificar')
+
+verificar.addEventListener('click', ()=>{
+
+    let nome = document.getElementById('nome').value
+    let idade = Number(document.getElementById('idade').value)
+    let titulo = Number(document.getElementById('titulo').value)
+
+    console.log(nome,idade,titulo)
+})
