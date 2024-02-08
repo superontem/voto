6fe710714ce73a25c2cd5db1d46bfa05c32435c5
Arthur <arthur_rosa2@estudante.sesisenai.org.br>
2024-02-08 11:54:58 -0300

    versão final do sistema de verificação de voto

--- voto_class.js
+++ voto_class.js
@@ -5,5 +5,22 @@ class Eleitor{
         this.titulo = titulo
     }
 
-    
+    verifcaVoto(){
+        let idade = this.idade
+
+        if(idade<16){
+            console.log("Abaixo da idade minima para votar")
+            return "Abaixo da idade minima para votar"
+        }else if((idade>=16)&&(idade<18)){
+    console.log("Idade onde o voto é facultativo")
+    return "Idade onde o voto é facultativo"
+            }else if((idade>=18)&&(idade<=70)){
+                console.log("Idade onde o voto é obrigatório")
+                return "Idade onde o voto é obrigatório"
+            }else if(idade > 70){
+                console.log("Idade onde o voto é facultativo")
+                return "Idade onde o voto é facultativo"
+            }
+            
+        }
 }
--- voto_main.js
+++ voto_main.js
@@ -16,4 +16,12 @@ verificar.addEventListener('click', ()=>{
     cidadao.titulo = titulo
 
     console.log("instancia do objeto: ", cidadao)
+
+   let mensagem = cidadao.verifcaVoto()
+
+   resposta.innerHTML = ''
+   resposta.innerHTML += `O eleitor ${cidadao.nome} <br> `
+   resposta.innerHTML += `tem ${cidadao.idade} anos de idade <br>`
+   resposta.innerHTML += mensagem + `<br>`
+   
 })
